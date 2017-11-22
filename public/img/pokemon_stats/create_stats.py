3d3e49627fc7b2e74bcb257f5510225acb7b5ae4
Passawit Riewthong <passawit1999@gmail.com>
2017-11-22 13:31:59 +0700

    ...

--- public/img/pokemon_stats/create_stats.py
+++ public/img/pokemon_stats/create_stats.py
@@ -4,6 +4,5 @@ import pygal
 
 df_pokemon = pd.read_csv('../pokemon_web/Pokemon.csv')
 for idx, data in df_pokemon.iterrows():
-    r = rq.get('https://img.pokemondb.net/artwork/{0}.jpg'.format((data.Name).lower()))
-    with open('{0}.jpg'.format((data.Name).lower()), 'wb') as f:
-        f.write(r.content)
+    radar_chart = pygal.Radar()
+    radar_chart.title = data.Name
