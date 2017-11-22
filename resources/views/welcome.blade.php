6f1200e8d8f456b1ae336d73f327673625f08b8e
Passawit Riewthong <passawit1999@gmail.com>
2017-11-22 22:30:12 +0700

    ..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,10 +112,10 @@
 
     </head>
     <body background="{{asset('img/poke_bg.jpg')}}">
-      <div class="topnav">
-        <a id="home" href="/">Home</a>
-        <a id="login" href="{{ route('login') }}">login</a>
-      </div>
+      <nav class="topnav">
+          <a id="home" href="/">Home</a>
+          <a id="login" href="{{ route('login') }}">login</a>
+      </nav>
       <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
       <section class="text-center" style="padding-top:4em;">
         <h1 style="font-weight: 600; color:#fff;">{{ $data->name }}</h1>
